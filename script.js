3a1f8c5776cc6b44a5a76a0b3f924281e2a5508f
asad1470 <160141504+asad1470@users.noreply.github.com>
2024-03-14 14:18:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded",()=>{
+//     const icon=document.querySelector("#icon");
+//     const listItems=document.querySelector("#listItems");
+//     console.log("Icon element:", icon);
+//     console.log("Navbar element:", listItems);
+//     icon.addEventListener('click',()=>{
+//         listItems.classList.toggle("show")
+//     });
+// });
+
+
